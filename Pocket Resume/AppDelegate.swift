c799e0a1136021640f4180dd3eaff2cb03330fbc
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-10-11 23:00:02 -0600

    Revert "Add new CoreData stack"
    This reverts commit 92ec5311a36c559107c97d7c61058461a69507cc.

--- Pocket Resume/AppDelegate.swift
+++ Pocket Resume/AppDelegate.swift
@@ -13,7 +13,6 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    private(set) lazy var coreDataStack = CoreDataStack(persistentStoreType: NSSQLiteStoreType)
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
@@ -40,7 +39,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        coreDataStack.saveMainContext()
+        self.saveContext()
     }
     
     // MARK: - Core Data stack
